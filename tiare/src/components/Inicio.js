bef24b9b4d247c4a08b673626148f571085abe92
Facundo <facundotisserasorribas@gmail.com>
2021-11-10 21:41:17 -0300

    creacion de Proxy localHost

--- tiare/src/components/Inicio.js
+++ tiare/src/components/Inicio.js
@@ -1,11 +1,33 @@
-
+import carrusel from '../assets/images/carrusel1.png'
 
 function Inicio() {
     return(
-    
-<h1>este es el inicio</h1>
-       
-   
-    )    
-};
+        
+        <div>
+            <main>
+            <img style={{width: 96 +'%'}} src={carrusel} alt="carrusel" />
+            <h2 className="titulo-productos">NUESTROS PRODUCTOS</h2>
+            <seccion className="secciones-articulos">
+                <article className="articulos-linea1">
+                    <div className="articulos-index">
+                        <div class="foto-producto">
+                            {/* <img className="foto-producto" style={{width: 96 +'%'}} src={algo} alt="Foto Producto Tiare" /> */}
+                        </div>
+                        <div className="descripciones">
+                            <p> Aca va la descripcion(nombre) producto</p>
+                        </div>
+                        <div className="precios">
+                            <p> Aca va el precio del producto</p>
+                        </div>
+                        <div className="loQuiero">
+                            <p>aca va el boton que redireccione al producto detalle</p>
+                        </div>
+                    </div>
+                </article>
+            </seccion>
+            </main>
+        </div>
+
+    );
+}
 export default Inicio;
---
+++ tiare/src/components/ListadoProductIndex.js
@@ -0,0 +1,14 @@
+import React, {component} from 'react';
+
+
+//aca voy a crear las apis que pegen en el inicio..
+
+class ListadoProductsIndex extends component{
+
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
+export default ListadoProductsIndex;
